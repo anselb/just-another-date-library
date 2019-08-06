117eaed90ac44e8d798935d77edefb40ae76dc0e
Ansel Bridgewater <iamansel@gmail.com>
2019-08-05 22:28:45 -0700

    Added babel for ES6 import in jest and tested import

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,10 @@
+import D from '../src/index';
+
 test('Sanity check', () => {
   expect(2 + 2).toBe(4);
+});
+
+test('D creats a new Date object', () => {
+  const d = new D(1565069098313);
+  expect(d.date.getTime()).toBe(1565069098313);
 });
