044addb6bfadf7db593df15b0ef1b75ac0481043
Ansel Bridgewater <iamansel@gmail.com>
2019-08-14 18:14:00 -0700

    Added tests for month(), day(), hour()

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ const currentDate = new D();
 const futureDate = new D('2468-10-12T14:16:18');
 
 test('D creats a new Date object', () => {
-  const unixTimestamp = 1565069098313
+  const unixTimestamp = 1565069098313;
   const d = new D(unixTimestamp);
   expect(d.date.getTime()).toBe(unixTimestamp);
 });
@@ -18,4 +18,23 @@ test('D returns the full year', () => {
   expect(pastDate.year()).toBe(1998);
   expect(currentDate.year()).toBe((new Date()).getFullYear());
   expect(futureDate.year()).toBe(2468);
+});
+
+test('D returns the month name', () => {
+  expect(pastDate.month()).toBe('June');
+  const currentMonthSlice = (currentDate.month()).slice(0, 3);
+  expect(currentMonthSlice).toBe((new Date()).toString().split(' ')[1]);
+  expect(futureDate.month()).toBe('October');
+});
+
+test('D returns the day of the month', () => {
+  expect(pastDate.day()).toBe(19);
+  expect(currentDate.day()).toBe((new Date()).getDate());
+  expect(futureDate.day()).toBe(12);
+});
+
+test('D returns the hour of the day, starting at 0', () => {
+  expect(pastDate.hour()).toBe(14);
+  expect(currentDate.hour()).toBe((new Date()).getHours());
+  expect(futureDate.hour()).toBe(14);
 });
